53c94885e6d4e0d01167e9066666179a59edcc06
Essamaraju Kiran <essamaraju.kiran@thoughtspot.com>
2024-07-07 12:08:30 +0530

    Add dark, light modes.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import theme from "./theme";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000",
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <ChakraProvider>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
       </ChakraProvider>
     </ApolloProvider>
