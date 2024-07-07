7cfe1409f4939cd12db826359a017eee91485f30
Essamaraju Kiran <essamaraju.kiran@thoughtspot.com>
2024-07-07 09:56:02 +0530

    Install and setup Chakra-ui.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { gql } from "./__generated__";
 import { useQuery } from "@apollo/client";
 import { StoryType } from "./__generated__/graphql";
+import { Button, Spinner } from "@chakra-ui/react";
 
 const STORIES = gql(`
   query GetStories($type: StoryType) {
@@ -20,11 +21,11 @@ function App() {
   const { loading, error, data } = useQuery(STORIES, {
     variables: { type: StoryType.Top }, // Here, 'type' is the variable name and 'top' is the value
   });
-  if (loading) return "Loading...";
+  if (loading) return <Spinner></Spinner>;
   if (error) return `Error! ${error.message}`;
   return (
     <>
-      <h1>Top Stories : </h1>
+      <Button color={"blue"}>Top Stories : </Button>
       <ul>
         {data?.getStories?.map((story) => (
           <li key={story.id}>{JSON.stringify(story)}</li>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
@@ -9,10 +9,12 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-    <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </ApolloProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
