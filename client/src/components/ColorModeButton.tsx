75887a075e6427687ed069b1ac791fa834d49a9f
Essamaraju Kiran <essamaraju.kiran@thoughtspot.com>
2024-07-07 12:43:34 +0530

    Build Color theme toggle button.

---
+++ client/src/components/ColorModeButton.tsx
@@ -0,0 +1,18 @@
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Button, HStack, useColorMode, Text } from "@chakra-ui/react";
+
+const ColorModeButton = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <>
+      <HStack>
+        <Text>Toggle mode</Text>
+        <Button onClick={toggleColorMode}>
+          {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+        </Button>
+      </HStack>
+    </>
+  );
+};
+
+export default ColorModeButton;
--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -9,45 +9,44 @@ import {
 import { ChevronRightIcon } from "@chakra-ui/icons";
 
 import logo from "../assets/logo.svg";
+import ColorModeButton from "./ColorModeButton";
 
-function NavBar() {
+const NavBar = () => {
   return (
     <>
-      <HStack>
-        <Image src={logo} boxSize="60px" borderRadius="20px"></Image>
+      <HStack padding="10px" justifyContent="space-between">
+        <HStack>
+          <Image src={logo} boxSize="60px" borderRadius="20px"></Image>
+          <Breadcrumb
+            fontWeight="medium"
+            spacing="10px"
+            separator={<ChevronRightIcon color="gray.500" />}
+          >
+            <BreadcrumbItem>
+              <BreadcrumbLink href="#">New</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="#">Top</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="#">Best</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="#">Ask</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="#">Show</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="#">Jobs</BreadcrumbLink>
+            </BreadcrumbItem>
+          </Breadcrumb>
+        </HStack>
 
-        <Breadcrumb
-          fontWeight="medium"
-          spacing="8px"
-          separator={<ChevronRightIcon color="gray.500" />}
-        >
-          <BreadcrumbItem>
-            <BreadcrumbLink href="#">New</BreadcrumbLink>
-          </BreadcrumbItem>
-
-          <BreadcrumbItem>
-            <BreadcrumbLink href="#">Top</BreadcrumbLink>
-          </BreadcrumbItem>
-
-          <BreadcrumbItem>
-            <BreadcrumbLink href="#">Best</BreadcrumbLink>
-          </BreadcrumbItem>
-
-          <BreadcrumbItem>
-            <BreadcrumbLink href="#">Ask</BreadcrumbLink>
-          </BreadcrumbItem>
-
-          <BreadcrumbItem>
-            <BreadcrumbLink href="#">Show</BreadcrumbLink>
-          </BreadcrumbItem>
-
-          <BreadcrumbItem>
-            <BreadcrumbLink href="#">Jobs</BreadcrumbLink>
-          </BreadcrumbItem>
-        </Breadcrumb>
+        <ColorModeButton />
       </HStack>
     </>
   );
-}
+};
 
 export default NavBar;
